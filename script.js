0a38b56ad4d9f507d984c787965a338aeb434e71
mdraihan27 <mdraihanhossen.cse@gmail.com>
2024-09-03 03:13:51 +0600

    final version 1.0

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const startBtn = document.getElementById('start-btn');
-let intervalId, count = 1, startBtnToggle = false;
+const iterationDesc = document.getElementById('iteration-desc');
+let sortBtnIntervalId, iterationDescIntervalId, count = 0, startBtnToggle = false;
 
 startBtn.addEventListener('click', function(){
+
     if(!startBtnToggle){
         bubbleSort();
         startBtnToggle = true;
+        showIterationDesc();
     }else{
         revert();
         startBtnToggle = false;
+        count = 0;
+        iterationDesc.innerText = '';
     }
 });
 
 function bubbleSort(){
+
    document.getElementById('num45').classList.add('animation-45');
    document.getElementById('num23').classList.add('animation-23');
    document.getElementById('num6').classList.add('animation-6');
@@ -19,8 +25,21 @@ function bubbleSort(){
    document.getElementById('num2').classList.add('animation-2');
    document.getElementById('num20').classList.add('animation-20');
 
-   intervalId = setInterval(changeBtnName, 16000);
+   iterationDesc.innerText = 'First iteration sorts 91'
 
+   sortBtnIntervalId = setInterval(changeBtnName, 16000);
+}
+
+function revert(){
+
+    document.getElementById('num45').classList.remove('animation-45');
+    document.getElementById('num23').classList.remove('animation-23');
+    document.getElementById('num6').classList.remove('animation-6');
+    document.getElementById('num91').classList.remove('animation-91');
+    document.getElementById('num2').classList.remove('animation-2');
+    document.getElementById('num20').classList.remove('animation-20');
+
+    sortBtnIntervalId = setInterval(changeBtnName, 1);
 }
 
 function changeBtnName(){
@@ -29,19 +48,44 @@ function changeBtnName(){
     }else{
         startBtn.innerText = 'BubbleSort'
     }
-    clearInterval(intervalId);
+    clearInterval(sortBtnIntervalId);
 }
 
+function showIterationDesc(){
 
-function revert(){
-    document.getElementById('num45').classList.remove('animation-45');
-    document.getElementById('num23').classList.remove('animation-23');
-    document.getElementById('num6').classList.remove('animation-6');
-    document.getElementById('num91').classList.remove('animation-91');
-    document.getElementById('num2').classList.remove('animation-2');
-    document.getElementById('num20').classList.remove('animation-20');
+    if(count === 0){
 
-    intervalId = setInterval(changeBtnName, 1);
+        iterationDesc.innerText = 'First iteration sorts 91';
+        clearInterval(iterationDescIntervalId);
+        iterationDescIntervalId = setInterval(showIterationDesc, 6000);
+
+    }else if(count === 1){
+
+        iterationDesc.innerText = iterationDesc.innerText + '\nSecond iteration sorts 45'
+        clearInterval(iterationDescIntervalId);
+        iterationDescIntervalId = setInterval(showIterationDesc, 4500);
+
+    }else if(count === 2){
+
+        iterationDesc.innerText = iterationDesc.innerText + '\nThird iteration sorts 20 and 23'
+        clearInterval(iterationDescIntervalId);
+        iterationDescIntervalId = setInterval(showIterationDesc, 3000);
+
+    }else if(count === 3){
+
+        iterationDesc.innerText = iterationDesc.innerText + '\nFourth iteration sorts 2 and 6'
+        clearInterval(iterationDescIntervalId);
+        iterationDescIntervalId = setInterval(showIterationDesc, 1500);
+
+    }else{
+
+        iterationDesc.innerText = iterationDesc.innerText + '\nSORTED!!!'
+        clearInterval(iterationDescIntervalId);
+
+    }
+
+    count++;
+
 }
 
 
